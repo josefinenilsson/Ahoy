e2f825a19dfccc33e5b73379810dae0198b2a21d
Gustav Lindqvist <gustav@glindqvist.se>
2013-12-28 22:14:32 +0100

    Added site logo and primary navigation
    Added site logo and primary navigation
    Added script for hiding site name on smaller screens
    Renamed elements.less to mixins.less
    Added Pirate Flag SVG as default logo

--- js/Ahoy.js
+++ js/Ahoy.js
@@ -3,13 +3,34 @@
    ###################################### */
 
 function captionPosition() {
-	$("#caption").css("top",String(($("#featured-image").height() - $("#caption").height() )/2) + "px");
+	$("#caption").css("margin-top","-"+String($("#caption").height()/2) + "px");
 }
+
+/* ######################################
+              Responsive Header
+   ###################################### */
+
+function responsiveHeader() {
+    console.log("responsiveHeader()");
+	if($("#header-wrapper").width() < ($("#site-title").width() + $("#site-navigation").width() + 10)) {
+        console.log("wrapper too narrow");
+        if (!($("#site-name").hasClass("hidden"))){
+            console.log("site-name hidden");
+            $("#site-name").addClass("hidden");
+        }
+    }
+}
+
+/* ######################################
+             Execution
+   ###################################### */
 
 $("#featured-image").load(function() {
     captionPosition();
+    responsiveHeader();
     $(window).resize(function() {
         captionPosition();
+        responsiveHeader();
     });
 });
 
