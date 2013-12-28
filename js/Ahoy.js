4237a9cafb5b9fc3fd9a3e99b2023ff925327ff7
Gustav Lindqvist <gustav@glindqvist.se>
2013-12-28 23:37:55 +0100

    Added design for widgets in footer

--- js/Ahoy.js
+++ js/Ahoy.js
@@ -7,30 +7,13 @@ function captionPosition() {
 }
 
 /* ######################################
-              Responsive Header
-   ###################################### */
-
-function responsiveHeader() {
-    console.log("responsiveHeader()");
-	if($("#header-wrapper").width() < ($("#site-title").width() + $("#site-navigation").width() + 10)) {
-        console.log("wrapper too narrow");
-        if (!($("#site-name").hasClass("hidden"))){
-            console.log("site-name hidden");
-            $("#site-name").addClass("hidden");
-        }
-    }
-}
-
-/* ######################################
              Execution
    ###################################### */
 
 $("#featured-image").load(function() {
     captionPosition();
-    responsiveHeader();
     $(window).resize(function() {
         captionPosition();
-        responsiveHeader();
     });
 });
 
