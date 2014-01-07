9858900ceaa43876ebd8efaa9c1a6ecfaae6c11b
Gustav Lindqvist <gustav@glindqvist.se>
2014-01-07 15:50:24 +0100

    Rewrote captionPosition() function
    Rewrote captionPosition() to be written in JavaScript instead of jQuery and wrote it so it can run on multiple captions on a single page.

--- js/Ahoy.js
+++ js/Ahoy.js
@@ -2,31 +2,21 @@
               Caption Position
    ###################################### */
 
-function captionPosition() {
-	$("#caption").css("margin-top","-"+String($("#caption").height()/2) + "px");
-}
-
-
-/* ######################################
-              Toggle Comments
-   ###################################### */
-
-function toggleComments() {
-	console.log("Toggle Comments");
+var captionPosition = function() { // Update the margin of every caption element so that it's perfectly positioned in the middle.
+    var caption = document.getElementsByClassName('caption');
+    for (var i = 0; i < caption.length; i++) {
+        var marginTop = "-"+String(caption[i].offsetHeight/2) + "px";
+        caption[i].style.marginTop = marginTop;
+    }
+	
 }
 
 /* ######################################
              Execution
    ###################################### */
 
-$("#toggle-comments").click(toggleComments);
-
-$("#featured-image").load(function() {
-    captionPosition();
-    $(window).resize(function() {
-        captionPosition();
-    });
-});
+window.onload=captionPosition; // Run on page load
+window.onresize=captionPosition; // Run on window resize
 
 
 
