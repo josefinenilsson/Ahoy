ace762e051cc172874b27b4c46b3032d79dc358b
Gustav Lindqvist <gustav@glindqvist.se>
2014-02-23 15:51:21 +0100

    Added responsive menu
    Added the responsive menu plugin responsive-nav.js

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <footer class="main-footer">
         <span class="powered">Proudly powered by <a href="http://sv.wordpress.org/">WordPress</a></span>
     </footer>
+    <script>
+        var nav = responsiveNav("#site-navigation");
+    </script>
 </body>
 </html>
